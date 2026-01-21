8682052995f107f8e87723fd1da906560c6e5a42
BekKing09 <satimovali91@gmail.com>
2026-01-21 12:02:47 +0500

    savol-javob oyini

--- main.py
+++ main.py
@@ -1,41 +1,48 @@
 import telebot
 from telebot.types import Message
-from googletrans import Translator
-
-
-translator = Translator()
-
-def translate_to(text: str, lang="uz"):
-    return translator.translate(text=text, dest=lang).text
-
 
 
 
 bot = telebot.TeleBot(token="bot token")
 
 
+
 @bot.message_handler(commands=["start"])
-def start_handler(message: Message):
-    bot.send_message(message.chat.id, f"Assalomu alaykum {message.from_user.full_name}!\n"
-                     "Men tarjimon botman. Istalgan matnni rus tiliga tarjima qilaman.\n"
-                     "Yuborilgan matnni shunchaki qaysi tilga tarjima qilish kerakligini yuboring!\n")
+def get_text(message: Message):
+    bot.send_message(message.chat.id, f"salom {message.from_user.full_name}\n"
+                                        f"menga savol bering javob beraman")
 
 
+
+
+
+
+
+
+questions = [
+    ["python nima", "python bu dasturlash tili"],
+    ["algoritm nima", "algoritm bu ketma ketlik"],
+    ["abdulloh AIdan ko'chiradimi", "ha albatta"],
+    ["isnming nima", "savol javob oyiniman"]
+]
+
+
+def get_question(text: str):
+    for i in questions:
+        if i[0] in text:
+            return i
+    return
 
 
 @bot.message_handler()
-def get_all_texts(message: Message):
+def get_text(message: Message):
+    answer = get_question(text=message.text)
 
-    translated_text = translate_to(message.text, lang="ru")
-
-    bot.send_message(message.chat.id, translated_text)
-
-
-
-
-
-
-
+    if not answer:
+        bot.send_message(message.chat.id, "Bunday savol topilmadi!")
+        return
+    
+    bot.send_message(message.chat.id, answer[1])
 
 
 
